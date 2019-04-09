0a10ccc031bdf2a560be60a70dc76b4419cd7100
felix01d <johnrodgers01d@gmail.com>
2019-04-08 23:06:20 -0500

    Untested code commited to change work stations.  Early work on MPINpArray structure.

--- mpids/MPInumpy.py
+++ mpids/MPInumpy.py
@@ -1,6 +1,15 @@
 from mpi4py import MPI
 import numpy as np
 
+class MPINpArray:
+    def __init__(self, np_array, comm = MPI.COMM_WORLD)
+        self.data = np_array.data
+        self.dtype = np_array.dtype
+        self.shape = np_array.shape
+        self.shape_global = None
+        self.strides = np_array.strides
+        self.comm = comm
+
 def dummy_function_for_commit_test():
         """Still configuring environment"""
         pass
