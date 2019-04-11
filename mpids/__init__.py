be8d9cc790f1c7bbaf96502d6dbd48067f3c1e28
felix01d <johnrodgers01d@gmail.com>
2019-04-11 18:36:48 -0500

    mpids/__init__: Adjusting import statement and specifying __all__

--- mpids/__init__.py
+++ mpids/__init__.py
@@ -1,4 +1,6 @@
-import ParallelIO
-import MPICounter
-import MPIArray
-import MPInumpy
+import mpids.ParallelIO
+import mpids.MPICounter
+import mpids.MPIArray
+import mpids.MPInumpy
+
+__all__ = ['ParallelIO', 'MPICounter', 'MPIArray', 'MPInumpy']
