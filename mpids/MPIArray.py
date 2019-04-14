1f6fb9ac7dc810345760702ff0cce2209bf390ae
felix01d <johnrodgers01d@gmail.com>
2019-04-13 22:13:57 -0500

    mpids/MPIArray: change of direction to try to emulate typical numpy array behavior

--- mpids/MPIArray.py
+++ mpids/MPIArray.py
@@ -3,21 +3,30 @@ from mpi4py import MPI
 class MPIArray:
         def __init__(self, np_array, comm=MPI.COMM_WORLD):
                 self.array = np_array
-                self.size_global = None
-                self.shape_global = None
                 self.comm = comm
+                self.size = np_array.size
+                self.data = np_array.data
+                self.dtype = np_array.dtype
+                self.shape = np_array.shape
+                self.strides = np_array.strides
+                self.comm_size = __find_global_size()
+                self.comm_shape = None
 
-        def size(self):
-                return self.array.size
+        def __find_global_size(self):
+                rank = self.comm.Get_rank()
+                self.comm.Allreduce(self.size, self.comm_size, op=MPI.SUM)
 
-        def data(self):
-                return self.array.data
-
-        def dtype(self):
-                return self.array.dtype
-
-        def shape(self):
-                return self.array.shape
-
-        def strides(self):
-                return self.array.strides
+        # def size(self):
+        #         return self.array.size
+        #
+        # def data(self):
+        #         return self.array.data
+        #
+        # def dtype(self):
+        #         return self.array.dtype
+        #
+        # def shape(self):
+        #         return self.array.shape
+        #
+        # def strides(self):
+        #         return self.array.strides
