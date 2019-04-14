b4fba41645d2b11dc24cc1ffc2fc11896adb80f0
felix01d <johnrodgers01d@gmail.com>
2019-04-13 22:46:03 -0500

    mpids/MPIArray: Updates based on cluster tests

--- mpids/MPIArray.py
+++ mpids/MPIArray.py
@@ -1,4 +1,5 @@
 from mpi4py import MPI
+import numpy as np
 
 class MPIArray:
         def __init__(self, np_array, comm=MPI.COMM_WORLD):
@@ -9,24 +10,10 @@ class MPIArray:
                 self.dtype = np_array.dtype
                 self.shape = np_array.shape
                 self.strides = np_array.strides
-                self.comm_size = __find_global_size()
+                self.comm_size = self.__find_comm_size()
                 self.comm_shape = None
 
-        def __find_global_size(self):
-                rank = self.comm.Get_rank()
-                self.comm.Allreduce(self.size, self.comm_size, op=MPI.SUM)
-
-        # def size(self):
-        #         return self.array.size
-        #
-        # def data(self):
-        #         return self.array.data
-        #
-        # def dtype(self):
-        #         return self.array.dtype
-        #
-        # def shape(self):
-        #         return self.array.shape
-        #
-        # def strides(self):
-        #         return self.array.strides
+        def __find_comm_size(self):
+                comm_size = np.array(0)
+                self.comm.Allreduce(np.array(self.size), comm_size, op=MPI.SUM)
+                return comm_size
