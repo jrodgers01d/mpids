c5849684a0c193adb3647e14f0b42cf6743ba2f3
felix01d <johnrodgers01d@gmail.com>
2019-10-14 10:52:49 -0500

    JSR: Initial unary operator implementations.

--- mpids/MPInumpy/MPIArray.py
+++ mpids/MPInumpy/MPIArray.py
@@ -178,4 +178,36 @@ class MPIArray(object):
         def __rxor__(self, other):
                 return getattr(self._array, '__rxor__')(other)
 
-# TODO: unary and comparison operators
+        #Unary Operations
+        def __neg__(self):
+                return getattr(self._array, '__neg__')()
+
+        def __pos__(self):
+                return getattr(self._array, '__pos__')()
+
+        def __abs__(self):
+                return getattr(self._array, '__abs__')()
+
+        def __invert__(self):
+                return getattr(self._array, '__invert__')()
+
+# TODO: remaining unary operators
+        # def __complex__(self):
+        #         return getattr(self._array, '__complex__')()
+        #
+        # def __int__(self):
+        #         return getattr(self._array, '__int__')()
+        #
+        # def __long__(self):
+        #         return getattr(self._array, '__long__')()
+        #
+        # def __float__(self):
+        #         return getattr(self._array, '__float__')()
+        #
+        # def __oct__(self):
+        #         return getattr(self._array, '__oct__')()
+        #
+        # def __hex__(self):
+        #         return getattr(self._array, '__hex__')()
+
+# TODO: comparison operators
--- mpids/MPInumpy/tests/MPIArray_test.py
+++ mpids/MPInumpy/tests/MPIArray_test.py
@@ -8,11 +8,9 @@ class MPIArrayTest(unittest.TestCase):
         def setUp(self):
                 self.comm = MPI.COMM_WORLD
                 self.comm_size = MPI.COMM_WORLD.Get_size()
-                self.data = list(range(4))
+                self.data = list(range(1,5))
                 self.np_array = np.array(self.data)
                 self.mpi_array = MPIArray(self.data, self.comm)
-
-
 
         def test_properties(self):
                 #Unique properties to MPIArray
@@ -21,11 +19,11 @@ class MPIArrayTest(unittest.TestCase):
                 self.assertEqual(self.comm_size * self.np_array.nbytes, self.mpi_array.globalnbytes)
 
                 #Replicated numpy.ndarray properties
-                self.assertEqual(self.np_array.T.tolist(), self.mpi_array.T.tolist())
+                self.assertTrue(np.alltrue(self.np_array.T == self.mpi_array.T))
                 self.assertEqual(self.np_array.data, self.mpi_array.data)
                 self.assertEqual(self.np_array.dtype, self.mpi_array.dtype)
-                self.assertEqual(self.np_array.imag.tolist(), self.mpi_array.imag.tolist())
-                self.assertEqual(self.np_array.real.tolist(), self.mpi_array.real.tolist())
+                self.assertTrue(np.alltrue(self.np_array.imag == self.mpi_array.imag))
+                self.assertTrue(np.alltrue(self.np_array.real == self.mpi_array.real))
                 self.assertEqual(self.np_array.size, self.mpi_array.size)
                 self.assertEqual(self.np_array.itemsize, self.mpi_array.itemsize)
                 self.assertEqual(self.np_array.nbytes, self.mpi_array.nbytes)
@@ -35,35 +33,49 @@ class MPIArrayTest(unittest.TestCase):
 
         def test_dunder_methods(self):
                 self.assertEqual('MPIArray', self.mpi_array.__repr__())
-                self.assertEqual('[0 1 2 3]', self.mpi_array.__str__())
-                self.assertEqual(self.np_array.tolist(),
-                                 self.mpi_array.__array__().tolist())
+                self.assertEqual('[1 2 3 4]', self.mpi_array.__str__())
+                self.assertTrue(np.alltrue(self.np_array == self.mpi_array.__array__()))
 
         def test_dunder_binary_operations(self):
-                self.assertEqual((self.np_array + 2).tolist(), (self.mpi_array + 2).tolist())
-                self.assertEqual((3 + self.np_array).tolist(), (3 + self.mpi_array).tolist())
-                self.assertEqual((self.np_array - 2).tolist(), (self.mpi_array - 2).tolist())
-                self.assertEqual((3 - self.np_array).tolist(), (3 - self.mpi_array).tolist())
-                self.assertEqual((self.np_array * 2).tolist(), (self.mpi_array * 2).tolist())
-                self.assertEqual((3 * self.np_array).tolist(), (3 * self.mpi_array).tolist())
-                self.assertEqual((self.np_array // 2).tolist(), (self.mpi_array // 2).tolist())
-                self.assertEqual((3 // self.np_array).tolist(), (3 // self.mpi_array).tolist())
-                self.assertEqual((self.np_array / 2).tolist(), (self.mpi_array / 2).tolist())
-                self.assertEqual((3 / self.np_array).tolist(), (3 / self.mpi_array).tolist())
-                self.assertEqual((self.np_array % 2).tolist(), (self.mpi_array % 2).tolist())
-                self.assertEqual((3 % self.np_array).tolist(), (3 % self.mpi_array).tolist())
-                self.assertEqual((self.np_array ** 2).tolist(), (self.mpi_array ** 2).tolist())
-                self.assertEqual((3 ** self.np_array).tolist(), (3 ** self.mpi_array).tolist())
-                self.assertEqual((self.np_array << 2).tolist(), (self.mpi_array << 2).tolist())
-                self.assertEqual((3 << self.np_array).tolist(), (3 << self.mpi_array).tolist())
-                self.assertEqual((self.np_array >> 2).tolist(), (self.mpi_array >> 2).tolist())
-                self.assertEqual((3 >> self.np_array).tolist(), (3 >> self.mpi_array).tolist())
-                self.assertEqual((self.np_array & 2).tolist(), (self.mpi_array & 2).tolist())
-                self.assertEqual((3 & self.np_array).tolist(), (3 & self.mpi_array).tolist())
-                self.assertEqual((self.np_array | 2).tolist(), (self.mpi_array | 2).tolist())
-                self.assertEqual((3 | self.np_array).tolist(), (3 | self.mpi_array).tolist())
-                self.assertEqual((self.np_array ^ 2).tolist(), (self.mpi_array ^ 2).tolist())
-                self.assertEqual((3 ^ self.np_array).tolist(), (3 ^ self.mpi_array).tolist())
+                self.assertTrue(np.alltrue((self.np_array + 2) == (self.mpi_array + 2)))
+                self.assertTrue(np.alltrue((3 + self.np_array) == (3 + self.mpi_array)))
+                self.assertTrue(np.alltrue((self.np_array - 2) == (self.mpi_array - 2)))
+                self.assertTrue(np.alltrue((3 - self.np_array) == (3 - self.mpi_array)))
+                self.assertTrue(np.alltrue((self.np_array * 2) == (self.mpi_array * 2)))
+                self.assertTrue(np.alltrue((3 * self.np_array) == (3 * self.mpi_array)))
+                self.assertTrue(np.alltrue((self.np_array // 2) == (self.mpi_array // 2)))
+                self.assertTrue(np.alltrue((3 // self.np_array) == (3 // self.mpi_array)))
+                self.assertTrue(np.alltrue((self.np_array / 2) == (self.mpi_array / 2)))
+                self.assertTrue(np.alltrue((3 / self.np_array) == (3 / self.mpi_array)))
+                self.assertTrue(np.alltrue((self.np_array % 2) == (self.mpi_array % 2)))
+                self.assertTrue(np.alltrue((3 % self.np_array) == (3 % self.mpi_array)))
+                self.assertTrue(np.alltrue((self.np_array ** 2) == (self.mpi_array ** 2)))
+                self.assertTrue(np.alltrue((3 ** self.np_array) == (3 ** self.mpi_array)))
+                self.assertTrue(np.alltrue((self.np_array << 2) == (self.mpi_array << 2)))
+                self.assertTrue(np.alltrue((3 << self.np_array) == (3 << self.mpi_array)))
+                self.assertTrue(np.alltrue((self.np_array >> 2) == (self.mpi_array >> 2)))
+                self.assertTrue(np.alltrue((3 >> self.np_array) == (3 >> self.mpi_array)))
+                self.assertTrue(np.alltrue((self.np_array & 2) == (self.mpi_array & 2)))
+                self.assertTrue(np.alltrue((3 & self.np_array) == (3 & self.mpi_array)))
+                self.assertTrue(np.alltrue((self.np_array | 2) == (self.mpi_array | 2)))
+                self.assertTrue(np.alltrue((3 | self.np_array) == (3 | self.mpi_array)))
+                self.assertTrue(np.alltrue((self.np_array ^ 2) == (self.mpi_array ^ 2)))
+                self.assertTrue(np.alltrue((3 ^ self.np_array) == (3 ^ self.mpi_array)))
+
+        def test_dunder_unary_operations(self):
+                np_scalar = np.array([1])
+                mpi_scalar = MPIArray([1], self.comm)
+
+                self.assertTrue(np.alltrue((-self.np_array) == (-self.mpi_array)))
+                self.assertTrue(np.alltrue((+self.np_array) == (+self.mpi_array)))
+                self.assertTrue(np.alltrue(abs(self.np_array) == abs(self.mpi_array)))
+                self.assertTrue(np.alltrue((~self.np_array) == (~self.mpi_array)))
+                # self.assertEqual(complex(np_scalar), complex(mpi_scalar))
+                # self.assertEqual(int(np_scalar), int(mpi_scalar))
+                # self.assertEqual(long(np_scalar), long(mpi_scalar))
+                # self.assertEqual(float(np_scalar), float(mpi_scalar))
+                # self.assertEqual(oct(np_scalar), oct(mpi_scalar))
+                # self.assertEqual(hex(np_scalar), hex(mpi_scalar))
 
 
 if __name__ == '__main__':
