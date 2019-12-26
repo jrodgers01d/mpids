a84f6ac8e65301d89e1ab84ca8e49e8594806d8e
felix01d <johnrodgers01d@gmail.com>
2019-12-26 13:22:24 -0600

    JSR: Refactoring array(creation) tests to emulate MPIArray testing framework

--- mpids/MPInumpy/tests/array_test.py
+++ mpids/MPInumpy/tests/array_test.py
@@ -4,45 +4,100 @@ from mpi4py import MPI
 import mpids.MPInumpy as mpi_np
 from mpids.MPInumpy.errors import InvalidDistributionError
 
-class ArrayTest(unittest.TestCase):
-
-        def setUp(self):
-                self.comm = MPI.COMM_WORLD
-                self.data = list(range(10))
-                self.data_2d = np.array(list(range(20))).reshape(5,4)
-                self.mpi_np_array = mpi_np.array(self.data, comm=self.comm)
-
-
+class ArrayCreationErrorsPropegatedTest(unittest.TestCase):
         def test_unsupported_distribution(self):
+                data = np.array(list(range(10)))
+                comm = MPI.COMM_WORLD
                 with self.assertRaises(InvalidDistributionError):
-                        mpi_np.array(self.data, comm=self.comm, dist='bananas')
+                        mpi_np.array(data, comm=comm, dist='bananas')
                 # Test cases where dim input data != dim distribution
                 with self.assertRaises(InvalidDistributionError):
-                        mpi_np.array(self.data, comm=self.comm, dist=('*', 'b'))
+                        mpi_np.array(data, comm=comm, dist=('*', 'b'))
                 with self.assertRaises(InvalidDistributionError):
-                        mpi_np.array(self.data, comm=self.comm, dist=('b','b'))
+                        mpi_np.array(data, comm=comm, dist=('b','b'))
 
 
-        def test_supported_distributions(self):
-                self.assertEqual(mpi_np.array(self.data, dist='u').dist, 'u')
-                self.assertEqual(mpi_np.array(self.data_2d, dist=('*', 'b')).dist, ('*', 'b'))
-                self.assertEqual(mpi_np.array(self.data_2d, dist=('b','b')).dist, ('b','b'))
+class ArrayDefaultTest(unittest.TestCase):
+        """ MPIArray creation routine.
+            See mpids.MPInumpy.tests.MPIArray_test for more exhaustive evaluation
+        """
+
+        def create_setUp_parms(self):
+                parms = {}
+                data = np.array(list(range(20))).reshape(5,4)
+                parms['comm'] = MPI.COMM_WORLD
+                # Default distribution
+                parms['dist'] = 'b'
+                parms['mpi_np_array'] = mpi_np.array(data, comm=parms['comm'])
+                return parms
+
+        def setUp(self):
+                parms = self.create_setUp_parms()
+                self.comm = parms['comm']
+                self.dist = parms['dist']
+                self.mpi_np_array = parms['mpi_np_array']
 
 
-        def test_default_behavior(self):
+        def test_return_behavior(self):
                 self.assertTrue(isinstance(self.mpi_np_array, mpi_np.MPIArray))
                 self.assertEqual(self.mpi_np_array.comm, self.comm)
-                self.assertEqual(self.mpi_np_array.dist, 'b')
+                self.assertEqual(self.mpi_np_array.dist, self.dist)
 
 
-        def test_array(self):
-                rank = self.comm.Get_rank()
-                rank_data_map = {0: [0, 1, 2],
-                                 1: [3, 4, 5],
-                                 2: [6, 7],
-                                 3: [8, 9]}
+class ArrayUndistributedTest(ArrayDefaultTest):
 
-                self.assertEqual(rank_data_map[rank], self.mpi_np_array.data.tolist())
+        def create_setUp_parms(self):
+                parms = {}
+                data = np.array(list(range(20))).reshape(5,4)
+                parms['comm'] = MPI.COMM_WORLD
+                # Undistributed distribution
+                parms['dist'] = 'u'
+                parms['mpi_np_array'] = mpi_np.array(data,
+                                                     comm=parms['comm'],
+                                                     dist=parms['dist'])
+                return parms
+
+
+class ArrayAltRowBlockTest(ArrayDefaultTest):
+
+        def create_setUp_parms(self):
+                parms = {}
+                data = np.array(list(range(20))).reshape(5,4)
+                parms['comm'] = MPI.COMM_WORLD
+                # Alternate row block distribution
+                parms['dist'] = ('b', '*')
+                parms['mpi_np_array'] = mpi_np.array(data,
+                                                     comm=parms['comm'],
+                                                     dist=parms['dist'])
+                return parms
+
+
+class ArrayColBlockTest(ArrayDefaultTest):
+
+        def create_setUp_parms(self):
+                parms = {}
+                data = np.array(list(range(20))).reshape(5,4)
+                parms['comm'] = MPI.COMM_WORLD
+                # Column block distribution
+                parms['dist'] = ('*', 'b')
+                parms['mpi_np_array'] = mpi_np.array(data,
+                                                     comm=parms['comm'],
+                                                     dist=parms['dist'])
+                return parms
+
+
+class ArrayBlockBlockTest(ArrayDefaultTest):
+
+        def create_setUp_parms(self):
+                parms = {}
+                data = np.array(list(range(20))).reshape(5,4)
+                parms['comm'] = MPI.COMM_WORLD
+                # Block block distribution
+                parms['dist'] = ('b', 'b')
+                parms['mpi_np_array'] = mpi_np.array(data,
+                                                     comm=parms['comm'],
+                                                     dist=parms['dist'])
+                return parms
 
 
 if __name__ == '__main__':
