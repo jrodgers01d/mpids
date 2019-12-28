aac66bc4bac8380bb87ba35c7c84679ee065dfc3
felix01d <johnrodgers01d@gmail.com>
2019-12-28 15:44:26 -0600

    JSR: Consistent formatting and removal of uncessary paths

--- pavement.py
+++ pavement.py
@@ -8,37 +8,37 @@ import shutil
 
 @task
 def test_mpinumpy():
-  sh('nosetests --exe --with-coverage --cover-erase --cover-branches --cover-package=mpids.MPInumpy mpids.MPInumpy.tests')
-  pass
+        sh('nosetests --exe --with-coverage --cover-erase --cover-branches --cover-package=mpids.MPInumpy')
+        pass
 
 @task
 def test_mpinumpy_html():
-  sh('nosetests --exe --with-coverage --cover-erase --cover-branches --cover-html --cover-package=mpids.MPInumpy mpids.MPInumpy.tests')
-  pass
+        sh('nosetests --exe --with-coverage --cover-erase --cover-branches --cover-html --cover-package=mpids.MPInumpy')
+        pass
 
 @task
 def clean():
-  for pycfile in glob.glob("*/*/*/*/*.pyc"): os.remove(pycfile)
-  for pycache in glob.glob("*/*/*/__pycache__"): os.removedirs(pycache)
-  for pycfile in glob.glob("*/*/*/*.pyc"): os.remove(pycfile)
-  for pycache in glob.glob("*/*/__pycache__"): os.removedirs(pycache)
-  for pycfile in glob.glob("*/*/*.pyc"): os.remove(pycfile)
-  for pycache in glob.glob("*/__pycache__"): os.removedirs(pycache)
-  for pycache in glob.glob("./__pycache__"): shutil.rmtree(pycache)
-  for coverfile in glob.glob(".coverage"): os.remove(coverfile)
-  for outfile in glob.glob("mpids*.out"): os.remove(outfile)
-  try:
-    shutil.rmtree(os.getcwd() + "/cover")
-  except:
-    pass
-  pass
+        for pycfile in glob.glob("*/*/*/*/*.pyc"): os.remove(pycfile)
+        for pycache in glob.glob("*/*/*/__pycache__"): os.removedirs(pycache)
+        for pycfile in glob.glob("*/*/*/*.pyc"): os.remove(pycfile)
+        for pycache in glob.glob("*/*/__pycache__"): os.removedirs(pycache)
+        for pycfile in glob.glob("*/*/*.pyc"): os.remove(pycfile)
+        for pycache in glob.glob("*/__pycache__"): os.removedirs(pycache)
+        for pycache in glob.glob("./__pycache__"): shutil.rmtree(pycache)
+        for coverfile in glob.glob(".coverage"): os.remove(coverfile)
+        for outfile in glob.glob("mpids*.out"): os.remove(outfile)
+        try:
+                shutil.rmtree(os.getcwd() + "/cover")
+        except:
+                pass
+        pass
 
 @task
 @needs(['clean', 'test_mpinumpy'])
 def default():
-  pass
+        pass
 
 @task
 @needs(['clean', 'test_mpinumpy_html'])
 def html_coverage_report():
-  pass
+        pass
