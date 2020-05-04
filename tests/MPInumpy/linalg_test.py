c2631cad653497d0237a0d5d392fcba8018fb5e4
felix01d <johnrodgers01d@gmail.com>
2020-05-04 13:11:53 -0500

    JSR: Updating MPInumpy linalg test to handle older versions of numpy on crill

--- tests/MPInumpy/linalg_test.py
+++ tests/MPInumpy/linalg_test.py
@@ -11,6 +11,14 @@ class LinAlgTest(unittest.TestCase):
         self.np_array_a = np.arange(16).reshape(4,4)
         self.np_array_b = np.arange(16).reshape(4,4) + 1
 
+    def check_numpy_supported_version():
+        #The numpy function 'matmul' was introduced in version 1.10...
+        major, minor, _ = [int(x) for x in np.version.version.split('.')]
+        return major > 0 and minor >= 10
+
+
+    @unittest.skipIf(not check_numpy_supported_version(),
+                     "Functionality missing from numpy version")
     def test_unsupported_functionality(self):
         #Use of 'out' field
         mpi_out = np.zeros(())
@@ -18,6 +26,8 @@ class LinAlgTest(unittest.TestCase):
             mpi_np.matmul(self.np_array_a, self.np_array_b, out=mpi_out)
 
 
+    @unittest.skipIf(not check_numpy_supported_version(),
+                     "Functionality missing from numpy version")
     def test_Replicated_matmul(self):
         mpi_array_a = mpi_np.array(self.np_array_a, dist='r')
         mpi_array_b = mpi_np.array(self.np_array_b, dist='r')
@@ -43,6 +53,9 @@ class LinAlgTest(unittest.TestCase):
             np.matmul(self.np_array_a, self.np_array_b) == \
             mpi_np.matmul(mpi_array_a, self.np_array_b)))
 
+
+    @unittest.skipIf(not check_numpy_supported_version(),
+                     "Functionality missing from numpy version")
     def test_block_distribution_matmul(self):
         rank = self.comm.Get_rank()
         mpi_array_a = mpi_np.array(self.np_array_a, dist='b')
@@ -54,6 +67,8 @@ class LinAlgTest(unittest.TestCase):
             mpi_np.matmul(mpi_array_a, mpi_array_b)))
 
 
+    @unittest.skipIf(not check_numpy_supported_version(),
+                     "Functionality missing from numpy version")
     def test_under_partitioned_block_distribution_matmul(self):
         #Current version of code will under partition a 2x8 matrix.
         #Want to make sure logic is sound with petsc4py.
